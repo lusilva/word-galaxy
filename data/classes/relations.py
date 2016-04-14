b58a46e9168be95e9aa11bd51593e37cb9b7b028
Lucas Silva <lsilvawd09@gmail.com>
2016-04-14 12:23:24 -0400

    initial commit

---
+++ data/classes/relations.py
@@ -0,0 +1,4 @@
+# Define all possible relations between words
+HYPERNYM = 'is_a'
+MEMBER_HOLONYM = 'member_of'
+PART_MERONYM = 'is_part_of'
---
+++ data/main.py
@@ -0,0 +1,31 @@
+from classes.dictionary import Dictionary
+import logging, sys
+from networkx.readwrite import json_graph
+import json
+import networkx as nx
+
+
+def main():
+    logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
+    # Set to either google-10000 or small-data
+    data_file = 'small-data'
+
+    # create a set of words from the input file
+    word_set = set()
+    with open(data_file + '.txt') as f:
+        for word in f:
+            word_set.add(word.rstrip())
+
+    # create a new dictionary with the words
+    dictionary = Dictionary(word_set)
+    graph = dictionary.get_graph()
+    # dictionary.calculate_spring_layout(100)
+    # plt.savefig("path.png")
+    # with open('../sigma/' + data_file + '.json', 'w') as outfile:
+    #     outfile.write(json.dumps(json_graph.node_link_data(graph)))
+    nx.write_gexf(graph, data_file + '.gexf')
+
+
+if __name__ == '__main__':
+    main()
